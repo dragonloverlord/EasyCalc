da1c19ffa252d4f7aa746837f4680f908cfe160f
dragonloverlord <dragonloverlord@gmail.com>
2014-11-21 12:53:38 -0500

    made improvements.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
+function id(id){
+  var rid = document.getElementById(id);
+  return rid;
+}
+
 function buttons(){
 
 }
 
 buttons.prototype.clear = function(){
-
+  var display = id("text");
+  display.innerHTML = "0";
 }
 
 buttons.prototype.one = function(){
-
+  var display = id("text");
+  if(display.innerHTML == "0"){
+    display.innerHTML = "1";
+  }else{
+    display.innerHTML += "1";
+  }
 }
